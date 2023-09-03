7ffdcd4e15dcbf923d6c74b5335a60148d002d2a
Arindam Dutta <arindam@ario-2.local>
2023-09-04 02:12:55 +0530

    config added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
  
 module.exports = {
-  basePath: '/github-pages',
+  basePath: 'arindamdutta.in',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
