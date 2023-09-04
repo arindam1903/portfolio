8f63a6a749c2a006e71eb1ce54afbaa46185cd16
Arindam Dutta <arindam@ario-2.local>
2023-09-05 03:23:31 +0530

    meta tags added

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,30 @@ export default function Home() {
   return (
     <div id='home_container'>
       <Head>
-        <title>Arindam Dutta</title>
+        <title>Arindam Dutta | Software Engineer</title>
+        <meta
+          name="description"
+          content="Arindam Dutta, Software Engineer in Gurgaon / Gurugram, Frontend developer at Trademo"
+          key="desc"
+        />
+        <meta property="og:title" content="Arindam Dutta, Software Engineer at Trademo" />
+        <meta
+          property="og:description"
+          content="Arindam is a Software engineer currently working at Trademo. He is a frontend developer with 2.5 years of experience. He lives in Gurgaon / Gurugram"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/deznixscd/image/upload/v1693749049/portfolio/arindam_dutta_software_engineer_frontend_developer_c0646h.png"
+        />
         <link rel="icon" href="/arindam_dutta_software_engineer_frontend_developer.ico" />
         <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
       </Head>
-        <Header />
-        <Intro />
-        <TechStack />
-        <About />
-        <Contact />
-        <Footer/>
+      <Header />
+      <Intro />
+      <TechStack />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   )
 }
