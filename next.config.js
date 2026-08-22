a085b8c275d2a7594d87488593e18d6e9ce0cf37
Arindam Dutta <arindamd@pm.me>
2026-08-22 22:35:26 +0530

    fix: node version updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
  
 module.exports = {
+  output: 'export',
   // basePath should only be used if your site is not served from the root path
   // basePath: '/subpath', // uncomment and modify if needed
   sassOptions: {
